6992b9968d41e5c7daac2aaa1c3c320be076b42d
deloooonp <delonpratama1009@gmail.com>
2026-09-06 13:54:51 +0700

    16 typescript setup

---
+++ 16-typescript-setup/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a;
+let b;
+function plus(a, b) {
+    const answer = a + b;
+    console.log(answer);
+    return answer;
+}
+plus(4, 5);
+//# sourceMappingURL=index.js.map
---
+++ 16-typescript-setup/src/index.ts
@@ -0,0 +1,10 @@
+let a;
+let b;
+
+function plus(a: number, b: number): number {
+  const answer = a + b;
+  console.log(answer);
+  return answer;
+}
+
+plus(4, 5);
